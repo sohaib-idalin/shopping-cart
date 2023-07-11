7c6fe02b804b81daf0c4b9445b72922d2cec9bb5
Nabil Kachar <kacharnabil1@gmail.com>
2023-07-11 16:49:25 +0100

    nav bar is updated

--- src/components/ProudctsList.jsx
+++ src/components/ProudctsList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useContext } from 'react'
 import { useCartDispatch,useCartProducts} from '../CartContext'
+import { useState } from 'react';
 
 
 
@@ -8,8 +9,9 @@ import { useCartDispatch,useCartProducts} from '../CartContext'
 function ProudctsList({product}) {
   const CartDispath=useCartDispatch();
   const cartProducts=useCartProducts();
-  
+ 
   const addHandler= ()=>{
+
 
   }
   
@@ -23,6 +25,7 @@ function ProudctsList({product}) {
                 <h3 className='font-medium p-2'>{product.name}</h3>
                 <p className=' text-gray-700 p-2'>{product.desc}</p>
                 <p className='font-medium text-center p-2'>{product.price} $</p>
+               
                 <button className='bg-gray-700 p-1 px-4 rounded-full my-2 text-white' onClick={addHandler} >Add To cart</button>
             </div>
 
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
+import {BsCart4} from 'react-icons/bs';
+
 export default function Layout() {
+  
+
+
   return (
     <>
       <div className='bg-black pb-5'>
 
-        <ul className='flex justify-end gap-8 pr-5 pt-4 text-white font-medium'>
+        <ul className='flex justify-end gap-8 items-center pr-5 pt-4 text-white font-medium'>
           <li>Home</li>
           <li>Products</li>
           <li>About Us</li>
+          <li className='text-5lg'><a href="./cart"><BsCart4/></a></li>
         </ul>
       </div>
       <Outlet />
