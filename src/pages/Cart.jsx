e4e2d6acfd2794b96bbc1fddcf9bc7d387ca8b0e
Nabil Kachar <kacharnabil1@gmail.com>
2023-07-12 10:52:53 +0100

    clear button is added

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,14 +3,20 @@ import { useCartProducts, useCartDispatch, } from '../CartContext'
 import CartProduct from '../components/CartProduct';
 
 export default function Cart() {
-  const cartProducts=useCartProducts()
-  const CartDispath=useCartDispatch();
+  const cartProducts = useCartProducts()
+  const CartDispath = useCartDispatch();
 
-    return (
-          <div className='text-center'>
-              <h1 className='text-xl font-bold m-4'>YOUR CART</h1>
-              <h2 className='text-gray-600 font-medium mb-10'>You have {cartProducts.length} products in your cart</h2>
-              {cartProducts.map((cartProduct)=>(<CartProduct key={cartProduct.product.id} product={cartProduct.product} amount={cartProduct.amount}/>))}
-          </div>
+  return (
+    <>
+      <div className='text-center'>
+        <h1 className='text-xl font-bold m-4'>YOUR CART</h1>
+        <h2 className='text-gray-600 font-medium mb-10'>You have {cartProducts.length} products in your cart</h2>
+      </div>
+      <div className='flex justify-end'>
+        <button className='mx-12 text-white p-2 px-5 bg-red-500 rounded-lg font-medium shadow-lg' onClick={handelClear}>Clear Your Cart</button>
+      </div>
+      {cartProducts.map((cartProduct) => (<CartProduct key={cartProduct.product.id} product={cartProduct.product} amount={cartProduct.amount} />))}
+
+    </>
   )
 }
