33bcddd5a3566424d15cac4da75696fc31744b7c
sohaib-idalin <sohaibidalin2017@gmail.com>
2023-07-12 14:37:40 +0100

    m25

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,12 +3,12 @@ import { useCartProducts, useCartDispatch, } from '../CartContext'
 import CartProduct from '../components/CartProduct';
 
 const shippingByArticl=2;
-
+const colorEnabled='mx-12 text-white p-2 px-5 bg-red-500 rounded-lg font-medium shadow-lg'
+const colorDisabled='mx-12 text-white p-2 px-5 bg-green-500 rounded-lg font-medium shadow-lg'
 export default function Cart() {
   const cartProducts = useCartProducts()
   const CartDispath = useCartDispatch();
-  const color = cartProducts.length ? ('mx-12 text-white p-2 px-5 bg-red-500 rounded-lg font-medium shadow-lg') :
-  ('mx-12 text-white p-2 px-5 bg-green-500 rounded-lg font-medium shadow-lg')
+  const color = cartProducts.length ? colorEnabled : colorDisabled
   const handelClear=()=>{
     CartDispath({type:'clear'})
   }
@@ -37,17 +37,17 @@ export default function Cart() {
   }
 
   return (
-    <div className='mb-[200px]'>
+    <div className='mb-[150px]'>
       <div className='text-center'>
         <h1 className='text-xl font-bold m-4'>YOUR CART</h1>
-        <h2 className='text-gray-600 font-medium mb-10'>You have {cartProducts.length} products in your cart</h2>
+        <h2 className='text-gray-600 font-medium mb-10'>You have <span className='font-bold text-green-500'>{cartProducts.length}</span> products in your cart</h2>
       </div>
       <div className='flex justify-end'>
         <button disabled={cartProducts.length===0} className={color} onClick={handelClear}>{cartProducts.length===0 ? ("Your cart is empty") : ("Clear your cart")}</button>
       </div>
       {cartProducts.map((cartProduct) => (<CartProduct key={cartProduct.product.id} product={cartProduct.product} amount={cartProduct.amount} />))}
       {cartProducts.length!==0 &&
-        <div className='sticky bottom-0 bg-gray-100 w-screen shadow-lg '>
+        <div className='fixed bottom-0 bg-gray-100 w-screen shadow-lg '>
           <div className='w-[50%] mx-auto px-10 py-2 leading-8 font-medium  '>
             <div>
               <div className='flex justify-between '>
