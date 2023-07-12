e20c3ee80fa39e247f320e2bc827ec0e28e865d0
sohaib-idalin <sohaibidalin2017@gmail.com>
2023-07-12 12:04:07 +0100

    m15

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useCartProducts, useCartDispatch, } from '../CartContext'
 import CartProduct from '../components/CartProduct';
 
+const shippingByArticl=2;
+
 export default function Cart() {
   const cartProducts = useCartProducts()
   const CartDispath = useCartDispatch();
@@ -9,9 +11,30 @@ export default function Cart() {
   const handelClear=()=>{
     CartDispath({type:'clear'})
   }
+  const subTotal=()=>{
+    if(cartProducts.length==0)return 0
+    let sum=0;
+    cartProducts.forEach(element => {
+      sum+=element.product.price*element.amount
+    });
+    return sum
+  }
+
+  const shippingCount =()=>{
+    if(cartProducts.length==0)return 0
+    let sum=0;
+    cartProducts.forEach(element => {
+      sum+=element.amount
+
+    });
+    return sum
+  }
+  const total=()=>{
+    return subTotal()+shippingCount()*shippingByArticl
+  }
 
   return (
-    <>
+    <div className='w-[100%]'>
       <div className='text-center'>
         <h1 className='text-xl font-bold m-4'>YOUR CART</h1>
         <h2 className='text-gray-600 font-medium mb-10'>You have {cartProducts.length} products in your cart</h2>
@@ -21,26 +44,26 @@ export default function Cart() {
       </div>
       {cartProducts.map((cartProduct) => (<CartProduct key={cartProduct.product.id} product={cartProduct.product} amount={cartProduct.amount} />))}
 
-      <div className='sticky bottom-0 bg-gray-100 w-screen- shadow-lg '>
+      <div className='sticky bottom-0 bg-gray-100 w-screen shadow-lg '>
         <div className='w-[50%] mx-auto px-10 py-2 leading-8 font-medium  '>
           <div>
             <div className='flex justify-between '>
               <div>Subtotal :</div>
-              <div> 4000</div>
+              <div> {subTotal()} $</div>
             </div>
             <div className='flex justify-between'>
               <div>Shipping fee :</div>
-              <div className='right-0'>5 * 17</div>
-              <div>85</div>
+              <div className='right-0'>2 * {shippingCount()} $</div>
+              <div>{2*shippingCount()} $</div>
             </div>
           </div>
           <div className='flex justify-between'>
             <div>Total :</div>
-            <div> 400</div>
+            <div> {total()} $</div>
           </div>
         </div>
       </div>
 
-    </>
+    </div>
   )
 }
