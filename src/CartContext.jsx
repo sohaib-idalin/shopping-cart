eba5420bb9f168a005e3e90910089e9ba3611e53
sohaib-idalin <sohaibidalin2017@gmail.com>
2023-07-12 10:25:59 +0100

    m8

--- src/CartContext.jsx
+++ src/CartContext.jsx
@@ -5,10 +5,12 @@ const CartProductsContext = createContext(null);
 
 const CartDispatchContext = createContext(null);
 
+const CartLocalKey='CartProducts'
+
 export function CartContext({ children }) {
   const [cartproducts, dispatch] = useReducer(
     cartProductReducer,
-    initialdata
+    readStorage()
   );
 
   return (
@@ -31,32 +33,39 @@ export function useCartDispatch() {
 function cartProductReducer(CartProducts, action) {
   switch (action.type) {
     case 'add': {
-        console.log('add')
-      return [...CartProducts, {
+      const res= [...CartProducts, {
         product: action.product,
         amount: 1
       }];
+      localStorage.setItem(CartLocalKey,JSON.stringify(res))
+      return res
     }
     case '+': {
-      return CartProducts.map(cp => {
+      const res= CartProducts.map(cp => {
         if (cp.product.id === action.product.id) {
           return {...cp , amount: cp.amount+1};
         } else {
           return cp;
         }
       });
+      localStorage.setItem(CartLocalKey,JSON.stringify(res))
+      return res
     }
     case '-': {
-        return CartProducts.map(cp => {
+      const res= CartProducts.map(cp => {
             if (cp.product.id === action.product.id) {
             return {...cp , amount: cp.amount-1};
             } else {
               return cp;
             }
           }).filter(cp => cp.amount !== 0);
+      localStorage.setItem(CartLocalKey,JSON.stringify(res))
+      return res
     }
     case 'delete': {
-        return CartProducts.filter(cp => cp.product.id !== action.product.id);
+      const res= CartProducts.filter(cp => cp.product.id !== action.product.id);
+      localStorage.setItem(CartLocalKey,JSON.stringify(res))
+      return res
     }
     default: {
       throw Error('Unknown action: ' + action.type);
@@ -64,7 +73,10 @@ function cartProductReducer(CartProducts, action) {
   }
 }
 
-const initialdata=[]
+const readStorage=()=>{
+  const res=localStorage.getItem(CartLocalKey)
+  return res? JSON.parse(res) : []
+}
 
 
 
